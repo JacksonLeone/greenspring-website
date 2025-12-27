bcab1735af3c8581d25ea3fb6545bdd1454a9214
JacksonLeone <jackson.leone.usa@gmail.com>
2025-12-27 17:02:49 -0400

    Fixing favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from './Navigation'
 import Footer from './Footer'
+import { getImagePath } from '../utils/imagePath';
 
 interface LayoutProps {
   children: React.ReactNode
@@ -14,12 +15,12 @@ export default function Layout({ children, title = 'GreenSpring - Business Trans
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/favicons/android-chrome-512x512.png" />
+        <link rel="icon" type="image/x-icon" href={getImagePath('favicons/favicon.ico')} />
+        <link rel="icon" type="image/png" sizes="16x16" href={getImagePath('favicons/favicon-16x16.png')} />
+        <link rel="icon" type="image/png" sizes="32x32" href={getImagePath('favicons/favicon-32x32.png')} />
+        <link rel="apple-touch-icon" sizes="180x180" href={getImagePath('favicons/apple-touch-icon-180x180.png')} />
+        <link rel="icon" type="image/png" sizes="192x192" href={getImagePath('favicons/android-chrome-192x192.png')} />
+        <link rel="icon" type="image/png" sizes="512x512" href={getImagePath('favicons/android-chrome-512x512.png')} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
